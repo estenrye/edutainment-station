f25b0cf90ad64c8f4e71d5d0077d5d7dbedd5fd2
Esten Rye <esten@ryezone.net>
2018-10-24 03:01:11 -0500

    Display works on the Pi.  NFC does not.

--- NFCBusyBox/MainPage.xaml.cs
+++ NFCBusyBox/MainPage.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Resources;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Networking.Proximity;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace EdutainmentStation
+namespace NFCBusyBox
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -27,7 +15,6 @@ namespace EdutainmentStation
     {
         private ProximityDevice proximityDevice;
         private CoreDispatcher coreDispatcher;
-        private ResourceLoader resourceLoader = new ResourceLoader();
 
         public MainPage()
         {
--- NFCBusyBox/StatusEnum.cs
+++ NFCBusyBox/StatusEnum.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace edutainment_station
+namespace NFCBusyBox
 {
     enum StatusEnum
     {
